2abf6f1cf51c375b542dc0d096ef203bb7fbd42c
Christopher Imrie <chris@moresoda.co.uk>
2012-09-07 12:17:05 +0100

    Fixed template output

--- system/expressionengine/third_party/routemaster/ft.routemaster.php
+++ system/expressionengine/third_party/routemaster/ft.routemaster.php
@@ -8,7 +8,7 @@ class Routemaster_ft extends EE_Fieldtype
 {
 	var $info = array(
 			"name"		=> "Routemaster",
-			"version"	=> "0.1"
+			"version"	=> "0.2"
 		);
 	var $js;
 
@@ -23,6 +23,21 @@ class Routemaster_ft extends EE_Fieldtype
 
 	public function display_field($data)
 	{
+        $data = unserialize(base64_decode($data));
+
+        if ($this->EE->extensions->active_hook('routemaster_display') === TRUE) {
+            $data = $this->EE->extensions->call('routemaster_display', $data);
+        }
+        
+        
+
+        if($data !== FALSE){
+            $data = json_encode($data);
+        } else {
+            $data = "";
+        }
+
+
 		$this->loadJs();
 		return "<div class='routemaster_field'>
 					<div class=\"map_canvas\" style=\"width: 100%; height: 100%\"></div>
@@ -35,12 +50,29 @@ class Routemaster_ft extends EE_Fieldtype
     function replace_tag($data, $params = array(), $tagdata = FALSE) {
         static $script_on_page = FALSE;
 
+
+        $data = unserialize(base64_decode($data));
+
+        if ($this->EE->extensions->active_hook('routemaster_display') === TRUE) {
+            $data = $this->EE->extensions->call('routemaster_display', $data);
+        }
+        
+        
+
+        if($data !== FALSE){
+            $data = json_encode($data);
+        } else {
+            $data = "";
+        }
+
+
+
         $ret = ""; 
 
         if(!$script_on_page){
-        $ret .= "
-                <link href='".URL_THIRD_THEMES."routemaster/css/styles.css' rel='stylesheet' type='text/javascript'/>
-                <script src=\"http://maps.googleapis.com/maps/api/js?sensor=false\" type='text/javascript'></script>";
+            $ret .= "<link href='".URL_THIRD_THEMES."routemaster/css/styles.css' rel='stylesheet' type='text/javascript'/>
+                    <script src=\"http://maps.googleapis.com/maps/api/js?sensor=false\" type='text/javascript'></script>";
+                
                 $this->EE->requirejs->add("third_party/routemaster/javascript/bootstrap");
                 $script_on_page = TRUE;
         }
@@ -49,6 +81,23 @@ class Routemaster_ft extends EE_Fieldtype
         // google maps javascript ...
 
         return $ret."<div class='routemaster_field'><div class=\"map_canvas\" style=\"width: 100%; height: 100%\"></div><textarea readonly class=\"map_state\">".$data."</textarea></div>";
+    }
+
+
+
+
+    public function save($data)
+    {
+        $data = json_decode($data);
+        
+
+        if ($this->EE->extensions->active_hook('routemaster_save') === TRUE) {
+            $data = $this->EE->extensions->call('routemaster_save', $data);
+        }
+
+        
+        $data = base64_encode((serialize($data)));
+        return $data;
     }
 
 
